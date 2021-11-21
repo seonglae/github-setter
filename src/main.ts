43c2e646c9ca9d93c7a304fb8560869ef91b8370
seonglae <sungle3737@gmail.com>
2021-11-21 18:36:06 +0900

    feat: repo label sync function

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { Octokit } from 'octokit'
 import { Command, Option, Cli, Builtins } from 'clipanion'
 import { version, name, displayName } from '../package.json'
 import { readdir, stat, readFile } from 'fs/promises'
+import githubLabelSync from 'github-label-sync'
+import { labels } from '../config/repo.json'
 
 const cli = new Cli({
   binaryName: name,
@@ -9,7 +11,7 @@ const cli = new Cli({
   binaryVersion: version
 })
 
-export class GithubSetCommand extends Command {
+export class GithubFolderCommand extends Command {
   static paths = [Command.Default]
   token = Option.String('-t,--token', {
     required: true,
@@ -22,18 +24,16 @@ export class GithubSetCommand extends Command {
   folder = Option.String('-f,--folder', '.github', {
     description: 'Folder to apply all repo'
   })
-
   async execute() {
     this.context.stdout.write(`\nSet all repo in ${this.user}\n`)
-
     const { rest } = new Octokit({ auth: this.token })
     const repositories = await rest.repos.listForUser({
       username: this.user
     })
+    const list = await nestedFiles(this.folder)
     for (const repository of repositories.data) {
-      this.context.stdout.write(repository.full_name + '\n')
       const [owner, repo] = repository.full_name.split('/')
-      const list = await nestedFiles(this.folder)
+      this.context.stdout.write(repository.full_name + '\n')
       for (const path of list) {
         const option = { owner, repo, path, sha: undefined }
         const content = await rest.repos.getContent(option).catch(() => null)
@@ -52,6 +52,30 @@ export class GithubSetCommand extends Command {
     }
   }
 }
+export class GithubLabelCommand extends Command {
+  static paths = [['labels']]
+  token = Option.String('-t,--token', {
+    required: true,
+    description: 'Github personal access token(PAT)'
+  })
+  user = Option.String('-u,--user', {
+    required: true,
+    description: 'User name to apply'
+  })
+  async execute() {
+    this.context.stdout.write(`\nSet all repo in ${this.user}\n`)
+    const { rest } = new Octokit({ auth: this.token })
+    const repositories = await rest.repos.listForUser({ username: this.user })
+    for (const repository of repositories.data) {
+      this.context.stdout.write(repository.full_name + '\n')
+      await githubLabelSync({
+        accessToken: this.token,
+        repo: repository.full_name,
+        labels
+      })
+    }
+  }
+}
 
 async function nestedFiles(folder, results = []) {
   const files = await readdir(folder)
@@ -63,6 +87,7 @@ async function nestedFiles(folder, results = []) {
   return results
 }
 
-cli.register(GithubSetCommand)
+cli.register(GithubFolderCommand)
+cli.register(GithubLabelCommand)
 cli.register(Builtins.HelpCommand)
 cli.runExit(process.argv.slice(3), Cli.defaultContext)
